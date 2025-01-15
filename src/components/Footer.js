e341a2e4a1111d836922225697a1bb144f6eb71a
Wanderson notebook <wandersonlife7@gmail.com>
2025-01-14 22:33:43 -0300

    creating the navbar and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <h3>Write about what you are interested in.</h3>
+      <p>Mini Blog &copy; 2025</p>
+    </footer>
+  );
+};
+
+export default Footer;
