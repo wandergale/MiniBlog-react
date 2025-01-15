165d0ae12639117098ff12c88b10ab7481f46f05
Wanderson notebook <wandersonlife7@gmail.com>
2025-01-14 22:17:59 -0300

    adding browserrouter, routes and route (navigate for use altter to)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+//CSS
 import "./App.css";
+
+// Routes
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 function App() {
   return (
     <div className="App">
-      <h1>MiniBlog</h1>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/About" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
