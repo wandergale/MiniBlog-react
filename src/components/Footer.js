871fc1250abd2c9a7253bb0e329bfcd67ad45040
Wanderson notebook <wandersonlife7@gmail.com>
2025-01-14 22:36:37 -0300

    footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
-import React from "react";
+// CSS
+import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <h3>Write about what you are interested in.</h3>
       <p>Mini Blog &copy; 2025</p>
     </footer>
