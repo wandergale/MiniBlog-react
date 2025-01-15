86cf67338df1b400e12576fb4c5e163954ab7b27
Wanderson notebook <wandersonlife7@gmail.com>
2025-01-15 16:18:59 -0300

    sign up user on system (firebase)

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -1,3 +1,5 @@
+import { db } from "../firebase/config";
+
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -18,7 +20,57 @@ export const useAuthentication = () => {
 
   const auth = getAuth();
 
-  function isCancelled() {
+  function checkIfIsCancelled() {
     if (cancelled) return;
   }
+
+  const createUser = async (data) => {
+    checkIfIsCancelled();
+
+    setLoading(true);
+
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+
+      await updateProfile(user, {
+        displayName: data.displayName,
+      });
+
+      setLoading(false);
+
+      return user;
+    } catch (error) {
+      console.log(error.message);
+      console.log(typeof error.message);
+
+      let systemErrorMessage;
+
+      if (error.message.includes("Password")) {
+        systemErrorMessage = "Password should be at least 6 characteres";
+      } else if (error.message.includes("email-already")) {
+        systemErrorMessage = "Email is already in use";
+      } else {
+        systemErrorMessage = "Something went wrong. Please try again later";
+      }
+
+      setError(systemErrorMessage);
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return {
+    auth,
+    createUser,
+    error,
+    loading,
+  };
 };
