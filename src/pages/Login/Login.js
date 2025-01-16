aba94119f12230e1ca69cc0b9c65688146b2447f
Wanderson notebook <wandersonlife7@gmail.com>
2025-01-15 22:09:14 -0300

    creating Login page front-end

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,68 @@
+// CSS
 import styles from "./Login.module.css";
 
+// hooks
+import { useState, useEffect } from "react";
+import { useAuthentication } from "../../hooks/useAuthentication";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const { error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError("");
+
+    const user = {
+      email,
+      password,
+    };
+  };
+
+  useEffect(() => {
+    if (authError) setError(authError);
+  }, [authError]);
+  return (
+    <div className={styles.about}>
+      <h1>Login</h1>
+      <p>Sign in to start using the system.</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="User e-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Password:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Create your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        {!loading && <button className="btn">Login</button>}
+        {loading && (
+          <button className="btn" disable>
+            Wait...
+          </button>
+        )}
+        {error && <p className="error">{error}</p>}
+      </form>
+    </div>
+  );
 };
 
 export default Login;
