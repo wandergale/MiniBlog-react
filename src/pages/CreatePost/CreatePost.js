fbebaa9237ae0472deea6b01e114e743343812d7
Wanderson notebook <wandersonlife7@gmail.com>
2025-01-16 11:38:27 -0300

    create post page and styles

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -1,9 +1,81 @@
+// CSS
 import styles from "./CreatePost.module.css";
 
+// hooks
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../context/AuthContext";
+
 const CreatePost = () => {
-  return <div>
-    <h1>Create Post</h1>
-  </div>;
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={styles.create_post}>
+      <h2>Create Post</h2>
+      <p>Write about anything</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Title:</span>
+          <input
+            type="text"
+            name="title"
+            required
+            placeholder="Write the title..."
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>Image url:</span>
+          <input
+            type="text"
+            name="image"
+            required
+            placeholder="Enter the URL of your image"
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+          />
+        </label>
+        <label>
+          <span>Content:</span>
+          <textarea
+            name="body"
+            required
+            placeholder="Enter the content of your post"
+            onChange={(e) => setBody(e.target.value)}
+            value={body}
+          ></textarea>
+        </label>
+        <label>
+          <span>Tags:</span>
+          <input
+            type="text"
+            name="tags"
+            required
+            placeholder="Enter the tags separated by commas"
+            onChange={(e) => setTags(e.target.value)}
+            value={tags}
+          />
+        </label>
+        <button className="btn">Create Post</button>
+        {/* {!loading && <button className="btn">Create Post</button>}
+      {loading && (
+        <button className="btn" disabled>
+          Wait...
+        </button>
+      )}
+      {error && <p className="error">{error}</p>} */}
+      </form>
+    </div>
+  );
 };
 
 export default CreatePost;
