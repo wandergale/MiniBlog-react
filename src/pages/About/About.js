6eac831784080477a358c6a7d901112ca6d07c99
Wanderson notebook <wandersonlife7@gmail.com>
2025-01-15 21:35:56 -0300

    creating about page and styling

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,22 @@
+// CSS
 import styles from "./About.module.css";
+
+// Route
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h2>
+        About the Mini <span>Blog</span>
+      </h2>
+      <p>
+        This project is a blog build with React on the front-end and Firebase on
+        the back-end
+      </p>
+      <Link to="/posts/create" className="btn">
+        Create post
+      </Link>
     </div>
   );
 };
