a45f22401abc8b4b4eb8eec35e8f81461b292e8d
Wanderson notebook <wandersonlife7@gmail.com>
2025-01-20 23:14:45 -0300

    creting styles on postDetail

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,13 +1,16 @@
+// CSS
+import styles from "./PostDetail.module.css";
+
 // hooks
 import { Link } from "react-router-dom";
 
 const PostDetail = ({ post }) => {
   return (
-    <div>
+    <div className={styles.post_detail}>
       <img src={post.image} alt={post.title} />
       <h2>{post.title}</h2>
-      <p>{post.createdBy}</p>
-      <div>
+      <p className={styles.createdBy}>{post.createdBy}</p>
+      <div className={styles.tags}>
         {post.tags.map((tag) => (
           <p key={tag}>
             <span>#</span>
