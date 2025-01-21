41907bf62792f0f3c7d34249cbb322ab9af30e48
Wanderson notebook <wandersonlife7@gmail.com>
2025-01-20 21:51:59 -0300

    Structuring the home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,42 @@
 //CSS
 import styles from "./Home.module.css";
 
+// hooks
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+
+// components
+
 const Home = () => {
+  const [query, setQuery] = useState("");
+  const [posts] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>New posts</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="Search for tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Search</button>
+      </form>
+      <div>
+        <h2>Posts</h2>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Oops! No posts found yet.</p>
+            <Link to="/posts/create" className="btn">
+              Create post
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
