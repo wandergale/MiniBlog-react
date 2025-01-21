a665b961963c929f47b00f4c98512dddf7317f88
Wanderson notebook <wandersonlife7@gmail.com>
2025-01-21 15:44:58 -0300

    adding styles

--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -10,11 +10,22 @@ const Post = () => {
   const { document: post, loading } = useFetchDocument("posts", id);
 
   return (
-    <div>
-        {loading && <p>Loading...</p>}
+    <div className={styles.post_container}>
+      {loading && <p>Loading...</p>}
       {post && (
         <>
           <h1>{post.title}</h1>
+          <img src={post.image} alt={post.title} />
+          <p>{post.body}</p>
+          <h3>This is about:</h3>
+          <div className={styles.tags}>
+            {post.tags.map((tag) => (
+              <p key={tag}>
+                <span>#</span>
+                {tag}
+              </p>
+            ))}
+          </div>
         </>
       )}
     </div>
