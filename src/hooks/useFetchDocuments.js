897fba4b058e670ca3579246831d783cfee5da44
Wanderson notebook <wandersonlife7@gmail.com>
2025-01-21 12:50:23 -0300

    fixing syntax erro when creating query

--- src/hooks/useFetchDocuments.js
+++ src/hooks/useFetchDocuments.js
@@ -32,7 +32,7 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
 
         if (search) {
           q = await query(
-            collection,
+            collectionRef,
             where("tags", "array-contains", search),
             orderBy("createdAt", "desc")
           );
