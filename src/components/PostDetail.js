5a0ae696ec25e5f1b0651b7f61a87711d9d5f53c
Wanderson notebook <wandersonlife7@gmail.com>
2025-01-20 23:05:56 -0300

    creating postDetails

---
+++ src/components/PostDetail.js
@@ -0,0 +1,25 @@
+// hooks
+import { Link } from "react-router-dom";
+
+const PostDetail = ({ post }) => {
+  return (
+    <div>
+      <img src={post.image} alt={post.title} />
+      <h2>{post.title}</h2>
+      <p>{post.createdBy}</p>
+      <div>
+        {post.tags.map((tag) => (
+          <p key={tag}>
+            <span>#</span>
+            {tag}
+          </p>
+        ))}
+      </div>
+      <Link to={`/posts/${post.id}`} className="btn btn-outline">
+        Read
+      </Link>
+    </div>
+  );
+};
+
+export default PostDetail;
