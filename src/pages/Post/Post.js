b4bf928c0bc6460e4d0c8fd57074f39747a80e68
Wanderson notebook <wandersonlife7@gmail.com>
2025-01-21 15:37:33 -0300

    src/pages/Post/Post.js

--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -3,10 +3,22 @@ import styles from "./Post.module.css";
 
 // hooks
 import { useParams } from "react-router-dom";
+import { useFetchDocument } from "../../hooks/useFetchDocument";
 
 const Post = () => {
   const { id } = useParams();
-  return <div>Post: {id}</div>;
+  const { document: post, loading } = useFetchDocument("posts", id);
+
+  return (
+    <div>
+        {loading && <p>Loading...</p>}
+      {post && (
+        <>
+          <h1>{post.title}</h1>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Post;
