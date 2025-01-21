c0eab5cb923502704a4395093375647d9af503f7
Wanderson notebook <wandersonlife7@gmail.com>
2025-01-21 15:12:05 -0300

    adding Post Route

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ import Footer from "./components/Footer";
 import CreatePost from "./pages/CreatePost/CreatePost";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Search from "./pages/Search/Search";
+import Post from "./pages/Post/Post";
 
 function App() {
   const [user, setUser] = useState(undefined);
@@ -51,6 +52,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/search" element={<Search />} />
+              <Route path="/posts/:id" element={<Post />} />
               <Route
                 path="/login"
                 element={!user ? <Login /> : <Navigate to="/" />}
