fe91555bafe57eb6ccb298c66b9cbcd3d22f90c7
Wanderson notebook <wandersonlife7@gmail.com>
2025-01-21 21:16:18 -0300

    creating dashboard logic

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,33 @@
+// CSS
 import styles from "./Dashboard.module.css";
 
+import { Link } from "react-router-dom";
+
+// hooks
+import { useAuthContext } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
 const Dashboard = () => {
+  const { user } = useAuthContext();
+  const uid = user.uid;
+
+  const posts = [];
   return (
     <div>
-      <h1>Create Post</h1>
+      <h2>Dashboard</h2>
+      <p>Manage your posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Any posts found...</p>
+          <Link to="/posts/create" className="btn">
+            Create post
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <p>Tem posts</p>
+        </div>
+      )}
     </div>
   );
 };
